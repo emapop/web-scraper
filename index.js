6c5bec8ab7f6c746b293dfaa679d253d1e50cad2
emapop <41983053+emapop@users.noreply.github.com>
2023-04-24 13:43:37 +0300

    made the web scraper all functional and the json data consistent

---
+++ index.js
@@ -0,0 +1,45 @@
+const PORT = 5000;
+const axios = require('axios');
+const cheerio = require('cheerio');
+const express = require('express');
+
+const app = express();
+
+let phone = 'samsung s21';
+var phoneQueriedOlx = phone.replace(/\s+/g, '-').toLowerCase();
+const urlSearchOlx = `https://www.olx.ro/electronice-si-electrocasnice/telefoane-mobile/cluj-napoca/q-${phoneQueriedOlx}/?currency=RON&search%5Border%5D=filter_float_price:asc`;
+
+axios(urlSearchOlx)
+    .then(response => {
+        try{
+            const html = response.data;
+            const $ = cheerio.load(html);
+            const articles = [];
+        $('.css-1sw7q4x', html).each(function(){
+           let title = $(this).find('.css-16v5mdi').text().trim();
+           let price = $(this).find('.css-10b0gli').children().remove().end().text();
+           let locationAndDate = $(this).find('.css-veheph').text().split(" ");
+           let urlRaw =  $(this).find('a').attr('href');
+           if (urlRaw == undefined) {
+            urlRaw = ''
+           }
+           let url = `https://www.olx.ro${urlRaw}`;
+           if (locationAndDate.length > 2) {
+                var location = locationAndDate[0];
+                var date = locationAndDate.slice(2).join('-');
+           }
+
+           if (price || date) {
+            articles.push({title, price, location, date, url})
+           }
+           
+            
+        })
+       console.log(articles);
+
+        }
+        catch (error) {
+            console.log(error);
+        }
+    })
+app.listen(PORT, () => console.log(`server running on PORT: ${PORT}`));
