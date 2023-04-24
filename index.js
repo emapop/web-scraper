3d8446364f17de7c2643b90c58784156f36f232f
emapop <41983053+emapop@users.noreply.github.com>
2023-04-24 22:39:39 +0300

    added the filter from the city and the for all of the country

--- index.js
+++ index.js
@@ -8,41 +8,63 @@ const app = express();
 
 const id = crypto.randomBytes(16).toString("hex");
 
-let phone = 'samsung s21';
-let priceMin = undefined;
-let priceMax = 1500;
+let city = "cluj-napoca";
+let cityQueriedOlx = city.replace(/\s+/g, "-").toLowerCase();
+let phone = "samsung s21";
+let priceMin = 150;
+let priceMax = undefined;
 let phoneQueriedOlx = phone.replace(/\s+/g, "-").toLowerCase();
-var priceFilterOlx = `&search%5Bfilter_float_price:from%5D=${priceMin}&search%5Bfilter_float_price:to%5D=${priceMax}&view=list`
-var urlSearchOlx = `https://www.olx.ro/electronice-si-electrocasnice/telefoane-mobile/cluj-napoca/q-${phoneQueriedOlx}/?currency=RON&search%5Border%5D=filter_float_price:asc`;
-//two apis for the normal and the filter
-if (priceMin > 0 && priceMax > 0) {
-    var urlSearchOlx = `https://www.olx.ro/electronice-si-electrocasnice/telefoane-mobile/cluj-napoca/q-${phoneQueriedOlx}/?currency=RON&search%5Border%5D=filter_float_price:asc${priceFilterOlx}`;
-}
-if (priceMin > 0 && priceMax === undefined) {
-var priceFilterOlx = `search%5Bfilter_float_price%3Afrom%5D=${priceMin}&search%5Border%5D=filter_float_price%3Aasc&view=list`;
-var urlSearchOlx = `https://www.olx.ro/electronice-si-electrocasnice/telefoane-mobile/cluj-napoca/q-${phoneQueriedOlx}/?currency=RON&${priceFilterOlx}`;
-}
+var priceFilterOlx = `&search%5Bfilter_float_price:from%5D=${priceMin}&search%5Bfilter_float_price:to%5D=${priceMax}&view=list`;
+var urlSearchOlx = `https://www.olx.ro/electronice-si-electrocasnice/telefoane-mobile/q-${phoneQueriedOlx}/?currency=RON&search%5Border%5D=filter_float_price:asc`;
+if (city) {
+  var urlSearchOlx = `https://www.olx.ro/electronice-si-electrocasnice/telefoane-mobile/${cityQueriedOlx}/q-${phoneQueriedOlx}/?currency=RON&search%5Border%5D=filter_float_price:asc`;
+  if (priceMin > 0 && priceMax > 0) {
+    var urlSearchOlx = `https://www.olx.ro/electronice-si-electrocasnice/telefoane-mobile/${cityQueriedOlx}/q-${phoneQueriedOlx}/?currency=RON&search%5Border%5D=filter_float_price:asc${priceFilterOlx}`;
+  }
+  if (priceMin > 0 && priceMax === undefined) {
+    var priceFilterOlx = `search%5Bfilter_float_price%3Afrom%5D=${priceMin}&search%5Border%5D=filter_float_price%3Aasc&view=list`;
+    var urlSearchOlx = `https://www.olx.ro/electronice-si-electrocasnice/telefoane-mobile/${cityQueriedOlx}/q-${phoneQueriedOlx}/?currency=RON&${priceFilterOlx}`;
+  }
 
-if (priceMin === undefined && priceMax > 0) {
+  if (priceMin === undefined && priceMax > 0) {
     var priceFilterOlx = `search%5Border%5D=filter_float_price:asc&search%5Bfilter_float_price:to%5D=${priceMax}&view=list`;
-    var urlSearchOlx = `https://www.olx.ro/electronice-si-electrocasnice/telefoane-mobile/cluj-napoca/q-${phoneQueriedOlx}/?currency=RON&${priceFilterOlx}`;
-    }
+    var urlSearchOlx = `https://www.olx.ro/electronice-si-electrocasnice/telefoane-mobile/${cityQueriedOlx}/q-${phoneQueriedOlx}/?currency=RON&${priceFilterOlx}`;
+  }
+}
+if (!city) {
+  if (priceMin > 0 && priceMax > 0) {
+    var urlSearchOlx = `https://www.olx.ro/electronice-si-electrocasnice/telefoane-mobile/q-${phoneQueriedOlx}/?currency=RON&search%5Border%5D=filter_float_price:asc${priceFilterOlx}`;
+  }
+  if (priceMin > 0 && priceMax === undefined) {
+    var priceFilterOlx = `search%5Bfilter_float_price%3Afrom%5D=${priceMin}&search%5Border%5D=filter_float_price%3Aasc&view=list`;
+    var urlSearchOlx = `https://www.olx.ro/electronice-si-electrocasnice/telefoane-mobile/q-${phoneQueriedOlx}/?currency=RON&${priceFilterOlx}`;
+  }
 
+  if (priceMin === undefined && priceMax > 0) {
+    var priceFilterOlx = `search%5Border%5D=filter_float_price:asc&search%5Bfilter_float_price:to%5D=${priceMax}&view=list`;
+    var urlSearchOlx = `https://www.olx.ro/electronice-si-electrocasnice/telefoane-mobile/q-${phoneQueriedOlx}/?currency=RON&${priceFilterOlx}`;
+  }
+}
+//two apis for the normal and the filter
 
 axios(urlSearchOlx).then((response) => {
   try {
     const html = response.data;
     const $ = cheerio.load(html);
     let articlesOlx = [];
-    const locationOlx =
-        {
-          location: "Olx",
-          articles: articlesOlx,
-        };
+    const locationOlx = {
+      location: "Olx",
+      articles: articlesOlx,
+    };
     $(".css-1sw7q4x", html).each(function () {
       let id_ = id;
       let title = $(this).find(".css-16v5mdi").text().trim();
-      let priceText = $(this).find(".css-10b0gli").children().remove().end().text();
+      let priceText = $(this)
+        .find(".css-10b0gli")
+        .children()
+        .remove()
+        .end()
+        .text();
       let price = priceText.split(" ").slice(0, -1).join();
       let moneda_schimb = priceText.split(" ").slice(-1).join();
       let locationAndDate = $(this).find(".css-veheph").text().split(" ");
@@ -57,7 +79,15 @@ axios(urlSearchOlx).then((response) => {
       }
 
       if (price || date) {
-        articlesOlx.push({id_, title, price, moneda_schimb, location, date, url });
+        articlesOlx.push({
+          id_,
+          title,
+          price,
+          moneda_schimb,
+          location,
+          date,
+          url,
+        });
       }
     });
     console.log(locationOlx);
