ad1f2bed35bdea482c5dc1551c2bdbb30218286b
emapop <41983053+emapop@users.noreply.github.com>
2023-04-24 14:31:52 +0300

    splited the priceText data and made a variable price and a currency variable named moneda_schimb and added them to the json structure as datas

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 let phone = 'samsung s21';
 var phoneQueriedOlx = phone.replace(/\s+/g, '-').toLowerCase();
-const urlSearchOlx = `https://www.olx.ro/electronice-si-electrocasnice/telefoane-mobile/cluj-napoca/q-${phoneQueriedOlx}/?currency=RON&search%5Border%5D=filter_float_price:asc`;
+const urlSearchOlx = `https://www.olx.ro/electronice-si-electrocasnice/telefoane-mobile/cluj-napoca/q-${phoneQueriedOlx}/?currency=RON&search%5Border%5D=filter_float_price:desc`;
 
 axios(urlSearchOlx)
     .then(response => {
@@ -17,7 +17,10 @@ axios(urlSearchOlx)
             const articles = [];
         $('.css-1sw7q4x', html).each(function(){
            let title = $(this).find('.css-16v5mdi').text().trim();
-           let price = $(this).find('.css-10b0gli').children().remove().end().text();
+           let priceText = $(this).find('.css-10b0gli').children().remove().end().text();
+           let price = priceText.split(' ').slice(0,-1).join();
+           let moneda_schimb = priceText.split(' ').slice(-1).join();
+           console.log(moneda_schimb, price)
            let locationAndDate = $(this).find('.css-veheph').text().split(" ");
            let urlRaw =  $(this).find('a').attr('href');
            if (urlRaw == undefined) {
@@ -30,7 +33,7 @@ axios(urlSearchOlx)
            }
 
            if (price || date) {
-            articles.push({title, price, location, date, url})
+            articles.push({title, price, moneda_schimb, location, date, url})
            }
            
             
